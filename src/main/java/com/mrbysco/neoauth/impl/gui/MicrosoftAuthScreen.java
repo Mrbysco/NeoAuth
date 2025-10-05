7be0bdff65f3d946ea566a444cf74a2874472493
Gravy Boat <gravy@thatgravyboat.tech>
2025-10-05 08:34:32 -0230

    feat: add copy link to clipboard button

--- src/main/java/com/mrbysco/neoauth/impl/gui/MicrosoftAuthScreen.java
+++ src/main/java/com/mrbysco/neoauth/impl/gui/MicrosoftAuthScreen.java
@@ -1,5 +1,6 @@
 package com.mrbysco.neoauth.impl.gui;
 
+import java.net.URI;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -8,10 +9,15 @@ import com.mrbysco.neoauth.NeoAuth;
 import com.mrbysco.neoauth.api.gui.AuthScreen;
 import com.mrbysco.neoauth.util.MicrosoftUtils;
 import com.mrbysco.neoauth.util.SessionUtils;
+import net.minecraft.Util;
+import net.minecraft.resources.ResourceLocation;
 import org.apache.http.conn.ConnectTimeoutException;
 import net.minecraft.ChatFormatting;
 import net.minecraft.client.gui.components.Button;
+import net.minecraft.client.gui.components.ImageButton;
 import net.minecraft.client.gui.components.StringWidget;
+import net.minecraft.client.gui.components.Tooltip;
+import net.minecraft.client.gui.components.WidgetSprites;
 import net.minecraft.client.gui.components.toasts.SystemToast;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.network.chat.Component;
@@ -20,12 +26,21 @@ import net.minecraft.network.chat.Component;
  * A screen for handling user authentication via Microsoft.
  */
 public class MicrosoftAuthScreen extends AuthScreen {
+
+    public static final WidgetSprites COPY_BUTTON_TEXTURES = new WidgetSprites(
+            ResourceLocation.fromNamespaceAndPath("neo_auth", "widget/copy_button"),
+            ResourceLocation.fromNamespaceAndPath("neo_auth", "widget/copy_button_disabled"),
+            ResourceLocation.fromNamespaceAndPath("neo_auth", "widget/copy_button_focused")
+    );
+
 	// The executor to run the login task on
 	private ExecutorService executor = null;
 	// The completable future for all Microsoft login tasks
 	private CompletableFuture<Void> task = null;
 	// The current progress/status of the login task
 	private StringWidget statusWidget = null;
+    // The uri used for authentication
+    private URI authUri = null;
 	// True if Microsoft should prompt to select an account
 	private final boolean selectAccount;
 
@@ -54,14 +69,28 @@ public class MicrosoftAuthScreen extends AuthScreen {
 		addRenderableWidget(titleWidget);
 
 		// Add a status message
+        var previousMessage = statusWidget != null ? statusWidget.getMessage() : Component.empty();
 		statusWidget = new StringWidget(width, height, title, font);
 		statusWidget.setColor(0xdddddd);
 		statusWidget.setPosition(width / 2 - statusWidget.getWidth() / 2, height / 2 - statusWidget.getHeight() / 2 - 1);
+        statusWidget.setMessage(previousMessage);
 		addRenderableWidget(statusWidget);
 
 		// Add a cancel button to abort the task
 		final Button cancelBtn;
-		addRenderableWidget(cancelBtn = Button.builder(Component.translatable("gui.cancel"), button -> onClose()).bounds(width / 2 - 50, height / 2 + 22, 100, 20).build());
+		addRenderableWidget(cancelBtn = Button.builder(Component.translatable("gui.cancel"), button -> onClose()).bounds(width / 2 - 50, height / 2 + 22, 75, 20).build());
+
+        final Button copyLinkBtn;
+		addRenderableWidget(copyLinkBtn = new ImageButton(width / 2 + 30, height / 2 + 22, 20, 20, COPY_BUTTON_TEXTURES, button -> copyLinkToClipboard(), Component.translatable("chat.copy")));
+        copyLinkBtn.setTooltip(Tooltip.create(
+                Component.translatable("gui.neo_auth.microsoft.button.copyLink")
+                        .append("\n")
+                        .append(
+                                Component.translatable("gui.neo_auth.microsoft.button.copyLink.tooltip").withStyle(ChatFormatting.GRAY)
+                        )
+        ));
+        copyLinkBtn.active = authUri != null;
+
 
 		// Prevent the task from starting several times
 		if (task != null) return;
@@ -75,7 +104,16 @@ public class MicrosoftAuthScreen extends AuthScreen {
 		// Start the login task
 		task = MicrosoftUtils
 				// Acquire a Microsoft auth code
-				.acquireMSAuthCode(success -> Component.translatable("gui.neo_auth.microsoft.browser").getString(), executor, selectAccount ? MicrosoftUtils.MicrosoftPrompt.SELECT_ACCOUNT : null)
+				.acquireMSAuthCode(
+                        uri -> {
+                            authUri = uri;
+                            copyLinkBtn.active = true;
+                            Util.getPlatform().openUri(uri);
+                        },
+                        success -> Component.translatable("gui.neo_auth.microsoft.browser").getString(),
+                        executor,
+                        selectAccount ? MicrosoftUtils.MicrosoftPrompt.SELECT_ACCOUNT : null
+                )
 
 				// Exchange the Microsoft auth code for an access token
 				.thenComposeAsync(msAuthCode -> {
@@ -133,6 +171,15 @@ public class MicrosoftAuthScreen extends AuthScreen {
 					return null; // return a default value
 				});
 	}
+
+    public void copyLinkToClipboard() {
+        if (authUri != null && minecraft != null) {
+            minecraft.keyboardHandler.setClipboard(authUri.toString());
+            if (statusWidget != null) {
+                statusWidget.setMessage(Component.translatable("gui.neo_auth.microsoft.status.linkCopied"));
+            }
+        }
+    }
 
 	@Override
 	public void onClose() {
